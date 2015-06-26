77952ee89c2aef1e1d172d1f84f18a8633aa2ff2
Earl <earl@titanweb.com.au>
2015-06-26 17:31:42 +0800

    icon 1 added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 $(function(){
 	$('#rnd-menu').load('rnd-menu.html', function() {
-		$('.'+$(this).data('active')).addClass('active');
+		$('.'+$(this).data('active')).parents('li').addClass('active');
+
+		$('#main_menu ul').each(function () {
+			var height = 0;
+			var elems = $(this).find('>li');
+			for(var y=0; y<elems.length ;y++)
+				height += elems.eq(y).outerHeight();
+			$(this).attr('data-height',height);
+		});
+
+		$('#main_menu li').hover(
+			function(){
+				var elem = $(this).find('ul');
+				elem.css('height', elem.data('height'));
+			},
+			function(){
+				var elem = $(this).find('ul');
+				elem.css('height', 0);
+			}
+		);
 	});
 });
